4914d11aacf9caaf07d4bbd54b8433f3ee1dfde4
xsl_lin@163.com <shuanglin0701@>
2019-04-13 16:40:09 +0800

    init product

---
+++ src/main/java/com/xsl/LinApplication.java
@@ -0,0 +1,13 @@
+package com.xsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinApplication.class, args);
+	}
+
+}
